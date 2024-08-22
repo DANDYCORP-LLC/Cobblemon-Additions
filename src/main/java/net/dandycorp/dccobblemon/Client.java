788d94a6d183fc867f44c522cb440a241cbd640c
dandy <thederpcow@gmail.com>
2024-08-22 00:22:18 -0600

    initial commit

---
+++ src/main/java/net/dandycorp/dccobblemon/Client.java
@@ -0,0 +1,12 @@
+package net.dandycorp.dccobblemon;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
